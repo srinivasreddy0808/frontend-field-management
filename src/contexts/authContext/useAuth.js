5ae61864631d538fabc8902e8dc326425e037bf3
srinivasreddy0808 <srinivasreddybrockk@gmail.com>
2025-01-14 17:27:06 +0530

    useAuth changed

--- src/contexts/authContext/useAuth.js
+++ src/contexts/authContext/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "./authContext";
+import AuthContext from "./AuthContext";
 const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
